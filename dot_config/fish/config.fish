9f5fd59f667a74353316890ed655a1273853389e
José Moraes <joseaugustomoraes@outlook.com>
2025-06-15 20:02:52 -0300

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,7 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
 
     mise activate fish | source
 
