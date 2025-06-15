5b62682e81b498edca18c349228eab5eaeb90068
José Moraes <joseaugustomoraes@outlook.com>
2025-06-15 14:45:55 -0300

    Add .config/fish/conf.d/path.fish Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,11 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-    fish_add_path "$HOME/.local/bin/"
-    fish_add_path "$HOME/bin/"
-    fish_add_path "$HOME/.bun/bin/"
-    fish_add_path "$HOME/.cargo/bin/"
-
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
     fzf --fish | source
