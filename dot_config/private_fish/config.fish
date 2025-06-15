bd87df146fe13cada97c9920f43559d606e4f1b7
José Moraes <joseaugustomoraes@outlook.com>
2025-06-15 14:34:54 -0300

    Update .config/chezmoi/chezmoi.toml Update .config/kitty/kitty.conf Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    fish_add_path "$HOME/.local/bin/"
+    fish_add_path "$HOME/bin/"
+    fish_add_path "$HOME/.bun/bin/"
+    fish_add_path "$HOME/.cargo/bin/"
+
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+    fzf --fish | source
+
     starship init fish | source
+
     enable_transience
 end
