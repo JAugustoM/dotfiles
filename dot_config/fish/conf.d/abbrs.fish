a68d09317219dd901f237f30166ad38b3f03c4fb
José Moraes <joseaugustomoraes@outlook.com>
2025-06-18 08:51:14 -0300

    Update .config/fish/conf.d/abbrs.fish

--- dot_config/fish/conf.d/abbrs.fish
+++ dot_config/fish/conf.d/abbrs.fish
@@ -2,7 +2,8 @@ if status is-interactive
     abbr --add picoserial "sudo minicom --device /dev/ttyACM0"
     abbr --add cn "cmake -G Ninja -B build -S ."
     abbr --add nb "ninja -C build"
-    abbr --add s --set-cursor "sudo %"
+    abbr --add s sudo
     abbr --add sdi --set-cursor "sudo dnf install %"
     abbr --add sdr --set-cursor "sudo dnf remove %"
+    abbr --add ez --set-cursor "eza -a -l -T -L 1% --git"
 end
